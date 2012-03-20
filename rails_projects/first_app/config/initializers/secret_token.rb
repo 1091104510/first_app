8ac5fc9be0839282d8508c983ac75105b514aaaa
Zhadyra Askarbekova <zhady_a_n@mail.ru>
2012-03-20 16:50:10 +0800

    first rails project

---
+++ rails_projects/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '2b7fb647202a9a1de9d94f8da5f5e7e71b2709e84bdb15b27fb3d01a3ee9e7298e22c41bd6f4cc4d3f3a76ba8496a7af1c05d59ac66384033d1cdd494a28ac0e'
---
+++ rails_projects/second_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = 'e2ed51b91b784dbcfa74cc20145357ec5289ec4ed48da50183c3252fc6f7c2231c8490936ae8dab8e8784c6e5f8419ea6cae536744e634911a4668dc924c00e9'
